df51f679207ad040d056a33a88033c74446b283d
komar <ignusius@gmail.com>
2011-04-22 15:56:28 +0400

    Base2

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 #-*-coding:utf-8-*-
-from bottle import route, run, debug, template, request, static_file, error,redirect
-import time
+from bottle import route, run, debug, template, request, static_file, error,redirect,PasteServer
 
+import time
 
 @route('/')
 def index():
@@ -10,7 +10,7 @@ def index():
 @route('/search', method='GET')
 def search():
     
-    output = template('mytemplate',button="search",date=request.GET.get('task1'),IP=request.GET.get('task2'),username=request.GET.get('task3'))
+    output = template('template/index',button="search",date=request.GET.get('task1'),IP=request.GET.get('task2'),username=request.GET.get('task3'))
     if request.GET.get('save','').strip():
         return output
         
@@ -24,11 +24,9 @@ def server_static(filename):
     return static_file(filename, root='static/images/')
 
  
- 
 @error(404)
 def Error404(code):
-    return "<h1>Страница 404</h1>" 
+    return "<h1>Страница 404 :(</h1>" 
 
 
-debug(True)
-run(host='192.168.1.128', port=8080,reloader=True)
+run(server=PasteServer(host='192.168.1.128'))
